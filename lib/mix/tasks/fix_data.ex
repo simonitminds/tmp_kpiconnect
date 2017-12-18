f36d666a3412876b2d27312014b606ea5cea452c
Jeremy Kahne <jkahne@gmail.com>
2017-12-18 13:07:21 -0500

    example Mix Task

---
+++ lib/mix/tasks/fix_data.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.FixData do
+  use Mix.Task
+
+  @shortdoc "foo"
+  def run("master") do
+    Mix.shell.info "master"
+  end
+
+  def run(_) do
+    Mix.shell.info "branch"
+  end
+end
