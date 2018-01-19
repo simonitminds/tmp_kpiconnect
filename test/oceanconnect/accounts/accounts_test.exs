8e6979c54314a15cbad30f46a85e0b8759eca7cf
Stamates <john@teamgaslight.com>
2018-01-19 11:31:59 -0500

    Refactor accounts_test to use factories not fixtures

--- test/oceanconnect/accounts/accounts_test.exs
+++ test/oceanconnect/accounts/accounts_test.exs
@@ -10,18 +10,16 @@ defmodule Oceanconnect.AccountsTest do
     @update_attrs %{email: "some updated email", password: "some updated password"}
     @invalid_attrs %{email: nil, password: nil}
 
-    def user_fixture(attrs \\ @valid_attrs) do
-      user = insert(:user, attrs)
-      %{user | password: nil}
+    setup do
+      user = insert(:user)
+      {:ok, %{user: Accounts.get_user!(user.id)}}
     end
 
-    test "list_users/0 returns all users" do
-      user = user_fixture()
+    test "list_users/0 returns all users", %{user: user} do
       assert Accounts.list_users() == [user]
     end
 
-    test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+    test "get_user!/1 returns the user with given id", %{user: user} do
       assert Accounts.get_user!(user.id) == user
     end
 
@@ -37,8 +35,7 @@ defmodule Oceanconnect.AccountsTest do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
     end
 
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
+    test "update_user/2 with valid data updates the user", %{user: user} do
       assert {:ok, user} = Accounts.update_user(user, @update_attrs)
       assert %User{} = user
       assert user.email == "some updated email"
@@ -47,20 +44,17 @@ defmodule Oceanconnect.AccountsTest do
       )
     end
 
-    test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
+    test "update_user/2 with invalid data returns error changeset", %{user: user} do
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       assert user == Accounts.get_user!(user.id)
     end
 
-    test "delete_user/1 deletes the user" do
-      user = user_fixture()
+    test "delete_user/1 deletes the user", %{user: user} do
       assert {:ok, %User{}} = Accounts.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
 
-    test "change_user/1 returns a user changeset" do
-      user = user_fixture()
+    test "change_user/1 returns a user changeset", %{user: user} do
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
@@ -72,22 +66,17 @@ defmodule Oceanconnect.AccountsTest do
     @update_attrs %{address1: "some updated address", contact_name: "some updated contact_name", country: "some updated country", name: "some updated name"}
     @invalid_attrs %{address1: nil, contact_name: nil, country: nil, name: nil}
 
-    def company_fixture(attrs \\ %{}) do
-      {:ok, company} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_company()
 
-      company
+    setup do
+      company = insert(:company)
+      {:ok, %{company: Accounts.get_company!(company.id)}}
     end
 
-    test "list_companies/0 returns all companies" do
-      company = company_fixture()
+    test "list_companies/0 returns all companies", %{company: company} do
       assert Accounts.list_companies() == [company]
     end
 
-    test "get_company!/1 returns the company with given id" do
-      company = company_fixture()
+    test "get_company!/1 returns the company with given id", %{company: company} do
       assert Accounts.get_company!(company.id) == company
     end
 
@@ -100,54 +89,50 @@ defmodule Oceanconnect.AccountsTest do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_company(@invalid_attrs)
     end
 
-    test "update_company/2 with valid data updates the company" do
-      company = company_fixture()
+    test "update_company/2 with valid data updates the company", %{company: company} do
       assert {:ok, company} = Accounts.update_company(company, @update_attrs)
       assert %Company{} = company
       assert all_values_match?(@update_attrs, company)
     end
 
-    test "update_company/2 with invalid data returns error changeset" do
-      company = company_fixture()
+    test "update_company/2 with invalid data returns error changeset", %{company: company} do
       assert {:error, %Ecto.Changeset{}} = Accounts.update_company(company, @invalid_attrs)
       assert company == Accounts.get_company!(company.id)
     end
 
-    test "delete_company/1 deletes the company" do
-      company = company_fixture()
+    test "delete_company/1 deletes the company", %{company: company} do
       assert {:ok, %Company{}} = Accounts.delete_company(company)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_company!(company.id) end
     end
 
-    test "change_company/1 returns a company changeset" do
-      company = company_fixture()
+    test "change_company/1 returns a company changeset", %{company: company} do
       assert %Ecto.Changeset{} = Accounts.change_company(company)
     end
 
-    test "add_port_to_company/2 with valid data" do
+    test "add_port_to_company/2 with valid data", %{company: company} do
       port = insert(:port)
-      company = :company |> insert |> Accounts.add_port_to_company(port)
-      assert company.ports == [port]
+      updated_company = company |> Accounts.add_port_to_company(port)
+      assert updated_company.ports == [port]
     end
 
-    test "add_port_to_company/2 with existing ports" do
+    test "add_port_to_company/2 with existing ports", %{company: company} do
       [port1, port2] = insert_list(2, :port)
-      company = :company |> insert |> Accounts.add_port_to_company(port1)
+      updated_company = company |> Accounts.add_port_to_company(port1)
       |> Accounts.add_port_to_company(port2)
-      assert Enum.all?(company.ports, fn(port) -> port in [port1, port2] end)
+      assert Enum.all?(updated_company.ports, fn(port) -> port in [port1, port2] end)
     end
 
-    test "set_ports_on_company/2 with valid data" do
+    test "set_ports_on_company/2 with valid data", %{company: company} do
       ports = insert_list(2, :port)
-      company = :company |> insert |> Accounts.set_ports_on_company(ports)
-      assert company.ports == ports
+      updated_company = company |> Accounts.set_ports_on_company(ports)
+      assert updated_company.ports == ports
     end
 
-    test "set_ports_on_company/2 overwriting existing ports" do
+    test "set_ports_on_company/2 overwriting existing ports", %{company: company} do
       [port1, port2] = insert_list(2, :port)
-      company = :company |> insert |> Accounts.set_ports_on_company([port1])
+      updated_company = company |> Accounts.set_ports_on_company([port1])
       |> Accounts.set_ports_on_company([port2])
-      assert company.ports == [port2]
+      assert updated_company.ports == [port2]
     end
 
   end
