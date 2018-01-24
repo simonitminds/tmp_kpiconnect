883bbc8e7de0becc64622fccfabc1a3fb52f95c8
James Smith <st23am@gmail.com>
2018-01-24 15:50:21 -0500

    Add current_user to SessionView

--- lib/oceanconnect_web/views/session_vew.ex
+++ lib/oceanconnect_web/views/session_vew.ex
@@ -1,4 +1,14 @@
 defmodule OceanconnectWeb.SessionView do
   use OceanconnectWeb, :view
 
+  def current_user(%Plug.Conn{assigns: %{current_user: user = %Oceanconnect.Accounts.User{}}}) do
+    user.email
+  end
+  def current_user(_conn), do: ""
+
+  def log_in_logout_link(conn) do
+    if current_user(conn) != "" do
+      link("Log Out", to: session_path(conn, :delete), method: :delete, class: "navbar-item")
+    end
+  end
 end
