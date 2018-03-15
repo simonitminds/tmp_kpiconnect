f851c8b89f99f06551c442ebed8e5b7f03b98a10
James Smith <st23am@gmail.com>
2018-03-15 16:37:44 -0400

    Hide status message when auction is not started.

--- assets/js/components/auction/SupplierWinningBid.js
+++ assets/js/components/auction/SupplierWinningBid.js
@@ -8,46 +8,48 @@ const SupplierWinningBid = ({auction}) => {
   const winningBid = _.get(auction, 'state.winning_bid');
   const order = _.get(auction, 'state.winning_bid_position');
   const bidStatsDisplay = () => {
-    if (bidList.length == 0) {
-      return <div className = "auction-notification box is-warning" >
-        <h3 className="has-text-weight-bold is-flex">
-        <span className="icon box__icon-marker is-medium has-margin-top-none">
-          <i className="fas fa-lg fa-adjust"></i>
-        </span>
-        <span className="is-inline-block qa-supplier-bid-status-message">You have not bid on this auction</span>
-        </h3>
-      </div>;
-    }
-    else if (order == 0) {
-      return <div className = "auction-notification box is-success" >
-        <h3 className="has-text-weight-bold is-flex">
-        <span className="icon box__icon-marker is-medium has-margin-top-none">
-          <i className="fas fa-lg fa-check-circle"></i>
-        </span>
-        <span className="is-inline-block qa-supplier-bid-status-message">Your bid is currently lowest</span>
-        </h3>
-      </div>;
-    } else {
-      if (order > 0) {
-        return
- <div className = "auction-notification box is-success" >
+    if(auction.state.status != "pending") {
+      if (bidList.length == 0) {
+        return <div className = "auction-notification box is-warning" >
+          <h3 className="has-text-weight-bold is-flex">
+          <span className="icon box__icon-marker is-medium has-margin-top-none">
+            <i className="fas fa-lg fa-adjust"></i>
+          </span>
+          <span className="is-inline-block qa-supplier-bid-status-message">You have not bid on this auction</span>
+          </h3>
+        </div>;
+      }
+      else if (order == 0) {
+        return <div className = "auction-notification box is-success" >
           <h3 className="has-text-weight-bold is-flex">
           <span className="icon box__icon-marker is-medium has-margin-top-none">
             <i className="fas fa-lg fa-check-circle"></i>
           </span>
-          <span className="is-inline-block qa-supplier-bid-status-message">You are in lowest bid position number {order + 1}</span>
+          <span className="is-inline-block qa-supplier-bid-status-message">Your bid is currently lowest</span>
           </h3>
         </div>;
       } else {
-        return
- <div className = "auction-notification box is-warning" >
-          <h3 className="has-text-weight-bold is-flex">
-          <span className="icon box__icon-marker is-medium has-margin-top-none">
-            <i className="fas fa-lg fa-times-circle"></i>
-          </span>
-          <span className="is-inline-block qa-supplier-bid-status-message">You have been outbid</span>
-          </h3>
-        </div>;
+        if (order > 0) {
+          return
+  <div className = "auction-notification box is-success" >
+            <h3 className="has-text-weight-bold is-flex">
+            <span className="icon box__icon-marker is-medium has-margin-top-none">
+              <i className="fas fa-lg fa-check-circle"></i>
+            </span>
+            <span className="is-inline-block qa-supplier-bid-status-message">You are in lowest bid position number {order + 1}</span>
+            </h3>
+          </div>;
+        } else {
+          return
+  <div className = "auction-notification box is-warning" >
+            <h3 className="has-text-weight-bold is-flex">
+            <span className="icon box__icon-marker is-medium has-margin-top-none">
+              <i className="fas fa-lg fa-times-circle"></i>
+            </span>
+            <span className="is-inline-block qa-supplier-bid-status-message">You have been outbid</span>
+            </h3>
+          </div>;
+        }
       }
     }
   }
