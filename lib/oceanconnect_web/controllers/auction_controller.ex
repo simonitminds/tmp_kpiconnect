f875274ff7cc8804a0dc372bbf5c8c46706398a6
James Smith <st23am@gmail.com>
2018-04-18 14:27:03 -0400

    Cleanup view logic from auction controller for log

--- lib/oceanconnect_web/controllers/auction_controller.ex
+++ lib/oceanconnect_web/controllers/auction_controller.ex
@@ -9,7 +9,6 @@ defmodule OceanconnectWeb.AuctionController do
   end
 
   def log(conn, %{"id" => id}) do
-
     current_company_id = OceanconnectWeb.Plugs.Auth.current_user(conn).company_id
     auction = id
     |> Auctions.get_auction!
@@ -21,7 +20,6 @@ defmodule OceanconnectWeb.AuctionController do
     do
       events = auction
       |> AuctionEventStore.event_list
-      |> make_data_renderable
 
       auction_payload = AuctionPayload.get_auction_payload!(auction, auction.buyer_id)
 
@@ -29,45 +27,6 @@ defmodule OceanconnectWeb.AuctionController do
     else
       _ -> redirect(conn, to: auction_path(conn, :index))
     end
-  end
-
-  defp make_data_renderable(events) do
-    Enum.map(events, fn(event) ->
-      updated_data = event.data |> convert_dates
-      event
-      |> Map.put(:data, updated_data |> Poison.encode!)
-      |> Map.put(:time, event.time_entered |> convert_date?)
-    end)
-  end
-
-  defp convert_dates(data = %{}) do
-    Enum.reduce(enumerate_data(data), %{}, fn({k, v}, acc) ->
-      Map.put(acc, k, convert_date?(v))
-    end)
-  end
-  defp convert_dates(list = []) do
-    Enum.map(list, fn(data) ->
-      convert_dates(data)
-    end)
-  end
-
-  defp enumerate_data(data = %{__struct__: _}) do
-    Map.from_struct(data)
-  end
-  defp enumerate_data(data), do: data
-
-  defp convert_date?(date_time = %DateTime{}) do
-    "#{leftpad(date_time.day)}/#{leftpad(date_time.month)}/#{date_time.year} #{leftpad(date_time.hour)}:#{leftpad(date_time.minute)}"
-  end
-  defp convert_date?(date_time = %NaiveDateTime{}) do
-    "#{leftpad(date_time.day)}/#{leftpad(date_time.month)}/#{date_time.year} #{leftpad(date_time.hour)}:#{leftpad(date_time.minute)}"
-  end
-  defp convert_date?(data = %{}), do: convert_dates(data)
-  defp convert_date?(list = []), do: convert_dates(list)
-  defp convert_date?(value), do: value
-
-  defp leftpad(integer) do
-    String.pad_leading(Integer.to_string(integer), 2, "0")
   end
 
   def start(conn, %{"id" => id}) do
