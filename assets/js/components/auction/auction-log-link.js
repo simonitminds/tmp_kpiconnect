8d320ae77e3ef51bc32d5b40737fb517a0ad7ac5
Lauren Woodrick <lauren@teamgaslight.com>
2018-05-01 14:56:15 -0400

    Tidy up Auction Report section a smidge.

--- assets/js/components/auction/auction-log-link.js
+++ assets/js/components/auction/auction-log-link.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const AuctionLogLink = ({auction}) => {
   return(
-    <div>
+    <div className="box">
+        <h3 className="box__header box__header--bordered has-margin-bottom-md">Auction Reports</h3>
       <a
-        className="button is-primary is-small has-family-copy is-capitalized"
+        className="button is-info has-family-copy is-capitalized"
         href={`/auctions/${auction.id}/log`}
       >
         Auction Log
