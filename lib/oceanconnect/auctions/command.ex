fb9695381899d2a917c97e7132ab70b3d3853159
Stamates <john@teamgaslight.com>
2018-05-16 13:18:57 -0400

    Add guards to command functions

--- lib/oceanconnect/auctions/command.ex
+++ lib/oceanconnect/auctions/command.ex
@@ -5,7 +5,7 @@ defmodule Oceanconnect.Auctions.Command do
 
   defstruct command: :get_current_state, data: nil
 
-  def update_auction(auction = %Auction{}, user) do
+  def update_auction(auction = %Auction{}, user) when not is_integer(user) do
     %Command{command: :update_auction, data: %{auction: auction, user: user}}
   end
 
@@ -21,11 +21,11 @@ defmodule Oceanconnect.Auctions.Command do
     %Command{command: :update_scheduled_start, data: auction}
   end
 
-  def cancel_scheduled_start(auction_id) do
+  def cancel_scheduled_start(auction_id) when is_integer(auction_id) do
     %Command{command: :cancel_scheduled_start, data: auction_id}
   end
 
-  def start_auction(auction = %Auction{}, user) do
+  def start_auction(auction = %Auction{}, user) when not is_integer(user) do
     %Command{command: :start_auction, data: %{auction: auction, user: user}}
   end
 
@@ -33,11 +33,11 @@ defmodule Oceanconnect.Auctions.Command do
     %Command{command: :end_auction, data: auction}
   end
 
-  def start_duration_timer(auction_id) do
+  def start_duration_timer(auction_id) when is_integer(auction_id) do
     %Command{command: :start_duration_timer, data: auction_id}
   end
 
-  def start_decision_duration_timer(auction_id) do
+  def start_decision_duration_timer(auction_id) when is_integer(auction_id) do
     %Command{command: :start_decision_duration_timer, data: auction_id}
   end
 
@@ -49,15 +49,15 @@ defmodule Oceanconnect.Auctions.Command do
     %Command{command: :enter_bid, data: bid}
   end
 
-  def process_new_bid(bid = %AuctionBid{}, user) do
+  def process_new_bid(bid = %AuctionBid{}, user) when not is_integer(user) do
     %Command{command: :process_new_bid, data: %{bid: bid, user: user}}
   end
 
-  def select_winning_bid(bid = %AuctionBid{}, user) do
+  def select_winning_bid(bid = %AuctionBid{}, user) when not is_integer(user) do
     %Command{command: :select_winning_bid, data: %{bid: bid, user: user}}
   end
 
-  def extend_duration(auction_id) do
+  def extend_duration(auction_id) when is_integer(auction_id) do
     %Command{command: :extend_duration, data: auction_id}
   end
 end
