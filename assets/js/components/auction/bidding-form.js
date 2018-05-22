b08fc8ae545f47378feae023cf2ebfdf511c800d
Lauren Woodrick <lauren@teamgaslight.com>
2018-05-22 13:23:07 -0400

    Make mobile bidding panel collapsible.

--- assets/js/components/auction/bidding-form.js
+++ assets/js/components/auction/bidding-form.js
@@ -1,33 +1,68 @@
 import React from 'react';
 import _ from 'lodash';
+import MediaQuery from 'react-responsive';
+import CollapsibleSection from './collapsible-section';
 
 const BiddingForm = ({auction, formSubmit}) => {
   const fuel = _.get(auction, 'fuel.name');
   return(
     <div className="auction-bidding box box--nested-base box--nested-base--base">
-      <form onSubmit={formSubmit.bind(this, auction.id)}>
-          <h3 className="title is-size-6 is-uppercase has-margin-top-sm">Place Bid</h3>
-          <div className="field is-horizontal is-expanded">
-            <div className="field-label">
-              <div className="control">
-                <label className="label" htmlFor="bid">Bid Amount</label>
+    <MediaQuery query="(min-width: 769px)">
+          <form onSubmit={formSubmit.bind(this, auction.id)}>
+              <h3 className="title is-size-6 is-uppercase has-margin-top-sm">Place Bid</h3>
+              <div className="field is-horizontal is-expanded">
+                <div className="field-label">
+                  <div className="control">
+                    <label className="label" htmlFor="bid">Bid Amount</label>
+                  </div>
+                </div>
+                <div className="field-body">
+                  <div className="control is-expanded has-icons-left">
+                    <input className="input qa-auction-bid-amount" type="number" id="bid" step="0.25" min="0" name="amount" />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-dollar-sign"></i>
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="field-body">
-              <div className="control is-expanded has-icons-left">
-                <input className="input qa-auction-bid-amount" type="number" id="bid" step="0.25" min="0" name="amount" />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-dollar-sign"></i>
-                </span>
+              <div className="field is-expanded is-grouped is-grouped-right has-margin-top-xs has-margin-bottom-sm">
+                <div className="control">
+                  <button type="submit" className="button is-primary qa-auction-bid-submit">Place Bid</button>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="field is-expanded is-grouped is-grouped-right has-margin-top-xs has-margin-bottom-sm">
-            <div className="control">
-              <button type="submit" className="button is-primary qa-auction-bid-submit">Place Bid</button>
-            </div>
-          </div>
-      </form>
+          </form>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 768px)">
+        <CollapsibleSection
+          trigger="Place Bid"
+          classParentString="collapsing-auction-bidding"
+          open={true}
+          >
+            <form onSubmit={formSubmit.bind(this, auction.id)}>
+                <h3 className="title is-size-6 is-uppercase has-margin-top-sm">Place Bid</h3>
+                <div className="field is-horizontal is-expanded">
+                  <div className="field-label">
+                    <div className="control">
+                      <label className="label" htmlFor="bid">Bid Amount</label>
+                    </div>
+                  </div>
+                  <div className="field-body">
+                    <div className="control is-expanded has-icons-left">
+                      <input className="input qa-auction-bid-amount" type="number" id="bid" step="0.25" min="0" name="amount" />
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-dollar-sign"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="field is-expanded is-grouped is-grouped-right has-margin-top-xs has-margin-bottom-sm">
+                  <div className="control">
+                    <button type="submit" className="button is-primary qa-auction-bid-submit">Place Bid</button>
+                  </div>
+                </div>
+            </form>
+        </CollapsibleSection>
+      </MediaQuery>
     </div>
   );
 };
--- assets/js/components/auction/show.js
+++ assets/js/components/auction/show.js
@@ -363,8 +363,10 @@ export default class AuctionShow extends React.Component {
 function updateAuctionBodySize() {
   const auctionHeaderSection = document.querySelector('.auction-app__header'),
         auctionHeaderOffsetHeight = auctionHeaderSection ? auctionHeaderSection.offsetHeight : 0,
+        collapsingBidBox = document.querySelector('.collapsing-auction-bidding'),
+        collapsingBidHeight = collapsingBidBox ? collapsingBidBox.offsetHeight : 0,
         auctionTabContentHeight = `calc(100vh - ${auctionHeaderOffsetHeight + 37 + 48}px)`,
-        auctionTabBidContentHeight = `calc(100vh - ${auctionHeaderOffsetHeight + 37 + 48 + 185}px)`,
+        auctionTabBidContentHeight = collapsingBidHeight ? `calc(100vh - ${auctionHeaderOffsetHeight + 37 + 48 + collapsingBidHeight}px)` : `calc(100vh - ${auctionHeaderOffsetHeight + 37 + 48}px)`,
         auctionTabWithAlertHeight = `calc(100vh - ${auctionHeaderOffsetHeight + 37 + 48 + 42}px)`,
         auctionTabContent = document.querySelector('.react-tabs__tab-panel--selected'),
         alertPresence = document.querySelector('.alert:not(:empty)'),
