5ac2d69af82e41460a7e6f58205f4d6bb229b6bd
Cam Carter <cam@gaslight.co>
2018-08-14 15:08:38 -0400

    Email unit tests are green

--- lib/oceanconnect/auctions/email.ex
+++ lib/oceanconnect/auctions/email.ex
@@ -2,115 +2,165 @@ defmodule Oceanconnect.Auctions.Email do
 	import Bamboo.Email
 
 	alias Oceanconnect.Accounts
-	alias Oceanconnect.Accounts.User
+  alias Oceanconnect.Accounts.Company
 	alias Oceanconnect.Auctions.Auction
 
-	def auction_invitation(user = %User{company_id: company_id}, %Auction{buyer_id: buyer_id}) do
-		case company_id == buyer_id do
-			true -> nil
-			false ->
-				base_email
-				|> to(user)
-				|> subject("You have been invited to an auction.")
-				|> html_body("<strong>Auction Invitation</strong>")
-				|> text_body("You have been invited to an auction on Oceanconnect Marine.")
-		end
+	def auction_invitation(%Auction{suppliers: supplier_companies}) do
+    suppliers = Accounts.users_for_companies(supplier_companies)
+
+    Enum.map(suppliers, fn(supplier) ->
+      base_email(supplier)
+      |> subject("You have been invited to an auction.")
+      |> html_body("<strong>Auction Invitation</strong>")
+      |> text_body("You have been invited to an auction on Oceanconnect Marine.")
+    end)
 	end
 
-	def auction_starting_soon(user = %User{}, %Auction{suppliers: supplier_companies, buyer_id: buyer_id}) do
+	def auction_starting_soon(%Auction{suppliers: supplier_companies, buyer_id: buyer_id}) do
 		buyer_company = Accounts.get_company!(buyer_id)
-		buyer = Accounts.users_for_companies([buyer_company])
+		buyers = Accounts.users_for_companies([buyer_company])
 		suppliers = Accounts.users_for_companies(supplier_companies)
 
-		supplier_email =
-			base_email
-		  |> bcc(suppliers)
-			|> subject("Auction starting soon.")
-			|> html_body("<strong>Auction Starting Soon</strong>")
-			|> text_body("An auction that you have been invited to is starting soon.")
-		buyer_email =
-		  base_email
-		  |> to(buyer)
-			|> subject("Your auction is starting soon.")
-			|> html_body("<strong>Auction Starting Soon</strong>")
-			|> text_body("Your auction is starting soon.")
+    # pass buyer_company as assigns in this
+		supplier_emails =
+			Enum.map(suppliers, fn(supplier) ->
+				base_email(supplier)
+				|> subject("Auction starting soon.")
+				|> html_body("<strong>Auction Starting Soon</strong>")
+				|> text_body("An auction that you have been invited to is starting soon.")
+			end)
+		buyer_emails =
+      Enum.map(buyers, fn(buyer) ->
+        base_email(buyer)
+        |> subject("Your auction is starting soon.")
+        |> html_body("<strong>Auction Starting Soon</strong>")
+        |> text_body("Your auction is starting soon.")
+      end)
 
-		case user in suppliers do
-			true -> supplier_email
-			false -> buyer_email
-		end
+     %{supplier_emails: supplier_emails, buyer_emails: buyer_emails}
 	end
 
-	def auction_starting(user) do
-		base_email
-		|> to(user)
-		|> subject("Auction starting.")
-		|> html_body("<strong>Auction Starting</strong>")
-		|> text_body("An auction that you have been invited to is starting.")
+	def auction_started(%Auction{suppliers: supplier_companies, buyer_id: buyer_id}) do
+		buyer_company = Accounts.get_company!(buyer_id)
+		buyers = Accounts.users_for_companies([buyer_company])
+		suppliers = Accounts.users_for_companies(supplier_companies)
+
+		supplier_emails =
+			Enum.map(suppliers, fn(supplier) ->
+				base_email(supplier)
+				|> subject("Auction started.")
+				|> html_body("<strong>Auction Started</strong>")
+				|> text_body("An auction that you have been invited to has started.")
+			end)
+		buyer_emails =
+      Enum.map(buyers, fn(buyer) ->
+        base_email(buyer)
+        |> subject("Your auction has started.")
+        |> html_body("<strong>Auction Started</strong>")
+        |> text_body("Your auction has started.")
+      end)
+
+    %{supplier_emails: supplier_emails, buyer_emails: buyer_emails}
 	end
 
-	def first_bid_placed(user) do
-		base_email
-		|> to(user)
-		|> subject("First bid placed.")
-		|> html_body("<strong>First Bid Placed</strong>")
-		|> text_body("The first bid has been placed in an auction you are participating.")
+	def auction_ending_soon(%Auction{suppliers: supplier_companies, buyer_id: buyer_id}) do
+		buyer_company = Accounts.get_company!(buyer_id)
+		buyers = Accounts.users_for_companies([buyer_company])
+		suppliers = Accounts.users_for_companies(supplier_companies)
+
+    # pass buyer_company as assigns in this
+		supplier_emails =
+			Enum.map(suppliers, fn(supplier) ->
+				base_email(supplier)
+				|> subject("Auction ending soon.")
+				|> html_body("<strong>Auction Ending Soon</strong>")
+				|> text_body("An auction that you have been invited to is ending soon.")
+			end)
+		buyer_emails =
+      Enum.map(buyers, fn(buyer) ->
+        base_email(buyer)
+        |> subject("Your auction is ending soon.")
+        |> html_body("<strong>Auction Ending Soon</strong>")
+        |> text_body("Your auction is ending soon.")
+      end)
+
+    %{supplier_emails: supplier_emails, buyer_emails: buyer_emails}
 	end
 
-	def losing_bid(user) do
-		base_email
-		|> to(user)
-		|> subject("You have been outbid.")
-		|> html_body("<strong>You have been outbid!</strong>")
-		|> text_body("Your highest bid has been beaten by another participant.")
+	def auction_ended(%Auction{suppliers: supplier_companies, buyer_id: buyer_id}) do
+		buyer_company = Accounts.get_company!(buyer_id)
+		buyers = Accounts.users_for_companies([buyer_company])
+		suppliers = Accounts.users_for_companies(supplier_companies)
+
+    # TODO: pass buyer_company as assigns in this
+		supplier_emails =
+			Enum.map(suppliers, fn(supplier) ->
+				base_email(supplier)
+				|> subject("Auction ended.")
+				|> html_body("<strong>Auction Ended</strong>")
+				|> text_body("An auction that you have been invited to has ended.")
+			end)
+		buyer_emails =
+      Enum.map(buyers, fn(buyer) ->
+        base_email(buyer)
+        |> subject("Your auction has ended.")
+        |> html_body("<strong>Auction Ended</strong>")
+        |> text_body("Your auction has ended.")
+      end)
+
+    %{supplier_emails: supplier_emails, buyer_emails: buyer_emails}
 	end
 
-	def winning_bid(user) do
-		base_email
-		|> to(user)
-		|> subject("You have the winning bid.")
-		|> html_body("<strong>You are in the lead!</strong>")
-		|> text_body("You are currently the highest bidder in the auction.")
-	end
+  def auction_closed(winning_supplier_company = %Company{}, %Auction{buyer_id: buyer_id}) do
+    buyer_company = Accounts.get_company!(buyer_id)
+    buyers = Accounts.users_for_companies([buyer_company])
+    suppliers = Accounts.users_for_companies([winning_supplier_company])
 
-	def tied_bid(user) do
-		base_email
-		|> to(user)
-		|> subject("Your bid is tied.")
-		|> html_body("<strong>You are tied for the lead!</strong>")
-		|> text_body("Your highest bid is currently tied with another participant's highest bid.")
-	end
+    supplier_emails =
+      Enum.map(suppliers, fn(supplier) ->
+        base_email(supplier)
+        |> subject("You have won the auction!")
+        |> html_body("<strong>Auction Won</strong>")
+        |> text_body("You have won the auction!")
+      end)
+    buyer_emails =
+      Enum.map(buyers, fn(buyer) ->
+        base_email(buyer)
+        |> subject("Your auction has closed.")
+        |> html_body("<strong>Auction Closed</strong>")
+        |> text_body("Your auction has closed.")
+      end)
 
-	def auction_ending_soon(user) do
-		base_email
-		|> to(user)
-		|> subject("Auction ending soon.")
-		|> html_body("<strong>Auction Ending Soon</strong>")
-		|> text_body("An auction that you are a participant in is ending soon.")
-	end
+    %{supplier_emails: supplier_emails, buyer_emails: buyer_emails}
+  end
 
-	def auction_ending(user) do
-		base_email
-		|> to(user)
-		|> subject("Auction ending.")
-		|> html_body("<strong>Auction Ending</strong>")
-		|> text_body("An auction that you are a participant in is ending.")
-	end
+  def auction_canceled(%Auction{suppliers: supplier_companies, buyer_id: buyer_id}) do
+		buyer_company = Accounts.get_company!(buyer_id)
+		buyers = Accounts.users_for_companies([buyer_company])
+		suppliers = Accounts.users_for_companies(supplier_companies)
 
-	def auction_won(user) do
-		base_email
-		|> to(user)
-		|> subject("Auction won!")
-		|> html_body("<strong>You have won the auction!</strong>")
-		|> text_body("Your bid was chosen as the best solution by the buyer.")
-	end
+    # TODO: pass buyer_company as assigns in this
+		supplier_emails =
+			Enum.map(suppliers, fn(supplier) ->
+				base_email(supplier)
+				|> subject("Auction canceled.")
+				|> html_body("<strong>Auction Canceled</strong>")
+				|> text_body("An auction that you have been invited to has been canceled.")
+			end)
+		buyer_emails =
+      Enum.map(buyers, fn(buyer) ->
+        base_email(buyer)
+        |> subject("Your auction has been canceled.")
+        |> html_body("<strong>Auction Canceled</strong>")
+        |> text_body("Your auction has canceled.")
+      end)
 
-	def auction_lost do
+    %{supplier_emails: supplier_emails, buyer_emails: buyer_emails}
+  end
 
-	end
-
-	defp base_email do
-		new_email
+	defp base_email(user) do
+		new_email()
 		|> from("oceanconnect@example.com")
+    |> to(user)
 	end
 end
--- test/oceanconnect/auctions/email_test.exs
+++ test/oceanconnect/auctions/email_test.exs
@@ -1,55 +1,133 @@
 defmodule Oceanconnect.EmailTest do
 	use Oceanconnect.DataCase
 
-	alias Oceanconnect.Auctions.{Auction, Email}
+	alias Oceanconnect.Auctions.Email
 	alias Oceanconnect.Accounts
 
 	setup do
 		auction = insert(:auction)
 		buyer_company = Accounts.get_company!(auction.buyer_id)
-		buyer = insert(:user, %{company: buyer_company})
+		buyers = [insert(:user, %{company: buyer_company}), insert(:user, %{company: buyer_company})]
 
 		supplier_companies = auction.suppliers
+    winning_supplier_company = Enum.at(Enum.take_random(supplier_companies, 1), 0)
+		Enum.each(supplier_companies, fn(supplier_company) ->
+			insert(:user, %{company: supplier_company})
+		end)
 		suppliers = Accounts.users_for_companies(supplier_companies)
-
-		{:ok, %{suppliers: suppliers, buyer: Accounts.get_user!(buyer.id), auction: auction}}
+    winning_suppliers = Accounts.users_for_companies([winning_supplier_company])
+		{:ok, %{suppliers: suppliers, buyers: buyers, auction: auction, winning_supplier_company: winning_supplier_company, winning_suppliers: winning_suppliers}}
 	end
 
 	describe "emails" do
-		test "auction invitation email builds for supplier", %{suppliers: suppliers, auction: auction} do
-			for supplier <- suppliers do
-				email = Email.auction_invitation(supplier, auction)
-
-				assert email.to == supplier
-				assert email.subject == "You have been invited to an auction."
-				assert email.html_body =~ "Auction Invitation"
+		test "auction invitation email builds for suppliers", %{suppliers: suppliers, auction: auction} do
+      supplier_emails = Email.auction_invitation(auction)
+			for supplier <- suppliers, do: assert Enum.any?(supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- supplier_emails do
+				assert supplier_email.subject == "You have been invited to an auction."
+				assert supplier_email.html_body =~ "Auction Invitation"
 			end
 		end
 
-		test "auction invitation email does not build for buyers", %{buyer: buyer, auction: auction} do
-			email = Email.auction_invitation(buyer, auction)
-
-			assert email == nil
+		test "auction invitation email does not build for buyers", %{buyers: buyers, auction: auction} do
+	    emails = Email.auction_invitation(auction)
+      for buyer <- buyers, do: refute Enum.any?(emails, fn(email) -> email.to.id == buyer.id end)
 		end
 
-		test "auction starting soon email builds for all participants", %{suppliers: suppliers, buyer: buyer, auction: auction} do
-			for supplier <- suppliers do
-				supplier_email = Email.auction_starting_soon(supplier, auction)
-
-				assert supplier_email.to == supplier
-				assert supplier_email.subject == "Auction starting soon."
+		test "auction starting soon email builds for all participants", %{suppliers: suppliers, buyers: buyers, auction: auction} do
+			supplier_emails = Email.auction_starting_soon(auction).supplier_emails
+			for supplier <- suppliers, do: assert Enum.any?(supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- supplier_emails do
+        assert supplier_email.subject == "Auction starting soon."
 				assert supplier_email.html_body =~ "Auction Starting Soon"
-			end
+      end
 
-			buyer_email = Email.auction_starting_soon(buyer, auction)
-
-			assert buyer_email.to == [buyer]
-			assert buyer_email.subject == "Your auction is starting soon."
-			assert buyer_email.html_body =~ "Auction Starting Soon"
-		 end
-
-		test "auction starting email builds for all participants", %{suppliers: suppliers, auction: auction} do
-			
+      buyer_emails = Email.auction_starting_soon(auction).buyer_emails
+      for buyer <- buyers, do: assert Enum.any?(buyer_emails, fn(buyer_email) -> buyer_email.to.id == buyer.id end)
+      for buyer_email <- buyer_emails do
+        assert buyer_email.subject == "Your auction is starting soon."
+        assert buyer_email.html_body =~ "Auction Starting Soon"
+      end
 		end
+
+		test "auction starting email builds for all participants", %{suppliers: suppliers, buyers: buyers, auction: auction} do
+      supplier_emails = Email.auction_started(auction).supplier_emails
+      for supplier <- suppliers, do: assert Enum.any?(supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- supplier_emails do
+				assert supplier_email.subject == "Auction started."
+				assert supplier_email.html_body =~ "Auction Started"
+      end
+
+			buyer_emails = Email.auction_started(auction).buyer_emails
+      for buyer <- buyers, do: assert Enum.any?(buyer_emails, fn(buyer_email) -> buyer_email.to.id == buyer.id end)
+      for buyer_email <- buyer_emails do
+			  assert buyer_email.subject == "Your auction has started."
+			  assert buyer_email.html_body =~ "Auction Started"
+      end
+		end
+
+    test "auction ending soon email builds for all participants", %{suppliers: suppliers, buyers: buyers, auction: auction} do
+      supplier_emails = Email.auction_ending_soon(auction).supplier_emails
+      for supplier <- suppliers, do: assert Enum.any?(supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- supplier_emails do
+				assert supplier_email.subject == "Auction ending soon."
+				assert supplier_email.html_body =~ "Auction Ending Soon"
+      end
+
+			buyer_emails = Email.auction_ending_soon(auction).buyer_emails
+      for buyer <- buyers, do: assert Enum.any?(buyer_emails, fn(buyer_email) -> buyer_email.to.id == buyer.id end)
+      for buyer_email <- buyer_emails do
+			  assert buyer_email.subject == "Your auction is ending soon."
+			  assert buyer_email.html_body =~ "Auction Ending Soon"
+      end
+    end
+
+    test "auction ending email builds for all participants", %{suppliers: suppliers, buyers: buyers, auction: auction} do
+      supplier_emails = Email.auction_ended(auction).supplier_emails
+      for supplier <- suppliers, do: assert Enum.any?(supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- supplier_emails do
+				assert supplier_email.subject == "Auction ended."
+				assert supplier_email.html_body =~ "Auction Ended"
+      end
+
+			buyer_emails = Email.auction_ended(auction).buyer_emails
+      for buyer <- buyers, do: assert Enum.any?(buyer_emails, fn(buyer_email) -> buyer_email.to.id == buyer.id end)
+      for buyer_email <- buyer_emails do
+			  assert buyer_email.subject == "Your auction has ended."
+			  assert buyer_email.html_body =~ "Auction Ended"
+      end
+    end
+
+    test "auction completion email builds for winning supplier and buyer", %{winning_supplier_company: winning_supplier_company, buyers: buyers, auction: auction, winning_suppliers: winning_suppliers} do
+      winning_supplier_emails= Email.auction_closed(winning_supplier_company, auction).supplier_emails
+      for supplier <- winning_suppliers, do: assert Enum.any?(winning_supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- winning_supplier_emails do
+        assert supplier_email.subject == "You have won the auction!"
+        assert supplier_email.html_body =~ "Auction Won"
+      end
+
+      buyer_emails = Email.auction_closed(winning_supplier_company, auction).buyer_emails
+      for buyer <- buyers, do: assert Enum.any?(buyer_emails, fn(buyer_email) -> buyer_email.to.id == buyer.id end)
+      for buyer_email <- buyer_emails do
+        assert buyer_email.subject == "Your auction has closed."
+        assert buyer_email.html_body =~ "Auction Closed"
+      end
+    end
+
+    test "auction cancellation email builds for all participants", %{suppliers: suppliers, buyers: buyers, auction: auction} do
+      supplier_emails = Email.auction_canceled(auction).supplier_emails
+      for supplier <- suppliers, do: Enum.any?(supplier_emails, fn(supplier_email) -> supplier_email.to.id == supplier.id end)
+      for supplier_email <- supplier_emails do
+        assert supplier_email.subject == "Auction canceled."
+        assert supplier_email.html_body =~ "Auction Canceled"
+      end
+
+			buyer_emails = Email.auction_canceled(auction).buyer_emails
+      for buyer <- buyers, do: assert Enum.any?(buyer_emails, fn(buyer_email) -> buyer_email.to.id == buyer.id end)
+      for buyer_email <- buyer_emails do
+			  assert buyer_email.subject == "Your auction has been canceled."
+			  assert buyer_email.html_body =~ "Auction Canceled"
+      end
+    end
 	end
 end
