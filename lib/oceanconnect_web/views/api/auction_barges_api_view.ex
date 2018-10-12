3fe5ab363576186e935cf725746c37a31b91de56
Jon <jonegeland@gmail.com>
2018-10-12 14:49:48 -0400

    Use AuctionPayload.json_from_payload in barges_api_view the whole point of refactoring it out...

--- lib/oceanconnect_web/views/api/auction_barges_api_view.ex
+++ lib/oceanconnect_web/views/api/auction_barges_api_view.ex
@@ -1,14 +1,10 @@
 defmodule OceanconnectWeb.Api.AuctionBargesView do
   use OceanconnectWeb, :view
 
+  alias Oceanconnect.Auctions.AuctionPayload
+
   def render("submit.json", %{auction_payload: auction_payload}) do
-    %{
-      time_remaining: auction_payload.time_remaining,
-      current_server_time: auction_payload.current_server_time,
-      auction: auction_payload.auction,
-      status: auction_payload.status,
-      submitted_barges: auction_payload.submitted_barges
-    }
+    AuctionPayload.json_from_payload(auction_payload)
   end
 
   def render("show.json", %{success: success, message: message}) do
