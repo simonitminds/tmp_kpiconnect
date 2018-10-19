f311d42e719d1830a066b3d90fd1049ff228d538
Jon <jonegeland@gmail.com>
2018-10-19 14:56:30 -0400

    Treat empty solutions as null objects for the front end. Display logic can now rely on the fact that a solution existing means that it has bids in it, allowing it to more easily hide and show elements as they are needed.

--- assets/js/components/auction/buyer-lowest-bid.js
+++ assets/js/components/auction/buyer-lowest-bid.js
@@ -12,16 +12,17 @@ const BuyerLowestBid = ({auctionPayload}) => {
     <div className="box">
       <div className="box__subsection has-padding-bottom-none">
         <h3 className="box__header box__header--bordered">Lowest Bid(s)</h3>
-        { bestSolution == undefined ?
-          <div>
-            <SolutionDisplay auctionPayload={auctionPayload} solution={bestSolution} isExpanded={true} title="Best Overall Offer" />
-            <SolutionDisplay auctionPayload={auctionPayload} solution={bestSingleSupplier} isExpanded={true} title="Best Single Supplier" />
-          </div> :
+        { bestSolution &&
+          <SolutionDisplay auctionPayload={auctionPayload} solution={bestSolution} isExpanded={true} title="Best Overall Offer" />
+        }
+        { bestSingleSupplier &&
+          <SolutionDisplay auctionPayload={auctionPayload} solution={bestSingleSupplier} isExpanded={true} title="Best Single Supplier" />
+        }
+        { !(bestSolution || bestSingleSupplier) &&
           <div className="auction-table-placeholder">
             <i>No bids have been placed on this auction</i>
           </div>
         }
-
       </div>
     </div>
   );
