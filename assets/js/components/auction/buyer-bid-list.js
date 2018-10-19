21d0ae840c1627b940165502a57ed890a8f6ef72
Jon <jonegeland@gmail.com>
2018-10-19 14:37:26 -0400

    Re-organize grade display for multi-product auctions. The buyer's grade display now shows bids organized by product, instead of having them all mixed together.
    Supplier's also now only see the `(Min: $...)` part of the amount when their bid has a `min_amount` value set.

--- assets/js/components/auction/buyer-bid-list.js
+++ assets/js/components/auction/buyer-bid-list.js
@@ -10,53 +10,47 @@ const BuyerBidList = ({auctionPayload, buyer}) => {
                    return(acc);
                  }, {})
                  .value();
-  const productBids = _.chain(auctionPayload)
-                       .get('product_bids')
-                       .map('lowest_bids')
-                       .flatten()
-                       .orderBy(['amount', 'time_entered'],['asc', 'asc'])
-                       .value();
-
-  const bidList = productBids;
-
-
-  const lowestBidIds = _.chain(auctionPayload)
-    .get('lowest_bids', [])
-    .map('id')
-    .value();
-
-  const lowestBid = (bidId) => {
-    if (_.includes(lowestBidIds, bidId)) {
-      return "is-selected";
-    }
-  }
+  const productBids = _.get(auctionPayload, 'product_bids');
+  const products = _.keys(productBids);
+  const bidList = _.chain(productBids)
+                   .map('lowest_bids')
+                   .flatten()
+                   .orderBy(['amount', 'time_entered'],['asc', 'asc'])
+                   .value();
 
   if (bidList.length > 0) {
     return(
       <div className="box">
         <h3 className="box__header box__header--bordered">Grade Display</h3>
-        <table className="table is-fullwidth is-striped is-marginless qa-auction-bids">
-          <thead>
-            <tr>
-              <th>Seller</th>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {_.map(bidList, ({id, amount, min_amount, fuel_id, is_traded_bid, time_entered, supplier}) => {
-              return (
-                <tr key={id} className={`${lowestBid(id)} qa-auction-bid-${id}`}>
-                  <td className="qa-auction-bid-supplier">{supplier}</td>
-                  <td className="qa-auction-bid-product">{fuels[fuel_id].name}</td>
-                  <td className="qa-auction-bid-amount">${formatPrice(amount)} <span className="qa-auction-bid-is_traded_bid">{is_traded_bid && <i action-label="Traded Bid" className="fas fa-exchange-alt has-margin-left-sm has-text-gray-3 auction__traded-bid-marker"></i>}</span></td>
-                  <td>{formatTime(time_entered)}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        { _.map(products, (fuel_id) => {
+            const lowest_bids = productBids[fuel_id].lowest_bids;
+
+            return (
+              <table className="table is-fullwidth is-striped is-marginless qa-auction-bids">
+                <thead>
+                  <tr>
+                    <th>{fuels[fuel_id].name}</th>
+                    <th>Price</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  { _.map(lowest_bids, ({id, amount, min_amount, fuel_id, is_traded_bid, time_entered, supplier}) => {
+                      return (
+                        <tr key={id} className={`qa-auction-bid-${id}`}>
+                          <td className="qa-auction-bid-supplier">{supplier}</td>
+                          <td className="qa-auction-bid-amount">${formatPrice(amount)} <span className="qa-auction-bid-is_traded_bid">{is_traded_bid && <i action-label="Traded Bid" className="fas fa-exchange-alt has-margin-left-sm has-text-gray-3 auction__traded-bid-marker"></i>}</span></td>
+                          <td>{formatTime(time_entered)}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table>
+            );
+          })
+        }
       </div>
     );
   }
