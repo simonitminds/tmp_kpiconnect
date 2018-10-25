130c7552a9538cc1a5bce34b1bc512ebefecc53b
Lauren Woodrick <lauren@teamgaslight.com>
2018-10-25 11:07:43 -0400

    Adapt bid box to mobile, reimplement collapsing.

--- assets/js/components/auction/bidding-form.js
+++ assets/js/components/auction/bidding-form.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import _ from 'lodash';
-import MediaQuery from 'react-responsive';
+import k from 'react-responsive';
 import CollapsibleSection from './collapsible-section';
 import { formatPrice } from '../../utilities';
 import CheckBoxField from '../check-box-field';
+import MediaQuery from 'react-responsive';
+
 
 const BiddingForm = ({auctionPayload, formSubmit, revokeBid, barges}) => {
   const auction = auctionPayload.auction;
@@ -81,40 +83,82 @@ const BiddingForm = ({auctionPayload, formSubmit, revokeBid, barges}) => {
 
   return(
     <div className={`auction-bidding ${auctionState == 'pending' ? 'auction-bidding--pending':''} box box--nested-base box--nested-base--base`}>
-      <form onSubmit={formSubmit.bind(this, auction.id)}>
-        <h3 className="auction-bidding__title title is-size-6 is-uppercase has-margin-top-sm">Place Bid</h3>
+      <MediaQuery query="(min-width: 769px)">
+        <form onSubmit={formSubmit.bind(this, auction.id)}>
+          <h3 className="auction-bidding__title title is-size-6 is-uppercase has-margin-top-sm">Place Bid</h3>
+          <div className="auction-bidding__form-body">
+            { (is_traded_bid_allowed === true) &&
+              <div className="field field--ribbon is-horizontal">
+                <div className="field-label"></div>
+                <div className="field-body">
+                  <CheckBoxField
+                    model={'auction-bid'}
+                    field={'is_traded_bid'}
+                    labelText={'mark as traded bid'}
+                    value={is_traded_bid}
+                    opts={{labelClass: 'label is-capitalized is-inline-block has-margin-left-sm'}}
+                  />
+                </div>
+              <i>Buyer's Credit Margin with OCM: $<span className="qa-auction-credit_margin_amount">{credit_margin_amount}</span></i>
+              </div>
+            }
 
-        { (is_traded_bid_allowed === true) &&
-          <div className="field field--ribbon is-horizontal">
-            <div className="field-label"></div>
-            <div className="field-body">
-              <CheckBoxField
-                model={'auction-bid'}
-                field={'is_traded_bid'}
-                labelText={'mark as traded bid'}
-                value={is_traded_bid}
-                opts={{labelClass: 'label is-capitalized is-inline-block has-margin-left-sm'}}
-              />
+            { products.map((product) => renderProduct(product, auctionPayload)) }
+          </div>
+          <div className="field field--offset is-horizontal">
+            <label className="checkbox">
+              <input className="has-margin-right-sm" type="checkbox" />
+                <strong>Do not split my bid</strong>
+            </label>
+          </div>
+          <div className="field is-horizontal is-expanded">
+            <div className="field is-expanded is-grouped is-grouped-right has-margin-top-xs has-margin-bottom-sm has-margin-left-auto">
+              <div className="control"><button type="submit" className="button is-primary qa-auction-bid-submit">Place Bid</button></div>
             </div>
-          <i>Buyer's Credit Margin with OCM: $<span className="qa-auction-credit_margin_amount">{credit_margin_amount}</span></i>
           </div>
-        }
+        </form>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 768px)">
+        <CollapsibleSection
+          trigger="Place Bid"
+          classParentString="collapsing-auction-bidding"
+          open={true}
+        >
+          <form onSubmit={formSubmit.bind(this, auction.id)}>
+            <h3 className="auction-bidding__title title is-size-6 is-uppercase has-margin-top-sm">Place Bid</h3>
+            <div className="auction-bidding__form-body">
+              { (is_traded_bid_allowed === true) &&
+                <div className="field field--ribbon is-horizontal">
+                  <div className="field-label"></div>
+                  <div className="field-body">
+                    <CheckBoxField
+                      model={'auction-bid'}
+                      field={'is_traded_bid'}
+                      labelText={'mark as traded bid'}
+                      value={is_traded_bid}
+                      opts={{labelClass: 'label is-capitalized is-inline-block has-margin-left-sm'}}
+                    />
+                  </div>
+                <i>Buyer's Credit Margin with OCM: $<span className="qa-auction-credit_margin_amount">{credit_margin_amount}</span></i>
+                </div>
+              }
 
-        { products.map((product) => renderProduct(product, auctionPayload)) }
-
-        <div className="field field--offset is-horizontal">
-          <label className="checkbox">
-            <input className="has-margin-right-sm" type="checkbox" />
-              <strong>Do not split my bid</strong>
-          </label>
-        </div>
-
-        <div className="field is-horizontal is-expanded">
-          <div className="field is-expanded is-grouped is-grouped-right has-margin-top-xs has-margin-bottom-sm has-margin-left-auto">
-            <div className="control"><button type="submit" className="button is-primary qa-auction-bid-submit">Place Bid</button></div>
-          </div>
-        </div>
-      </form>
+              { products.map((product) => renderProduct(product, auctionPayload)) }
+            </div>
+            <div className="field field--offset is-horizontal">
+              <label className="checkbox">
+                <input className="has-margin-right-sm" type="checkbox" />
+                  <strong>Do not split my bid</strong>
+              </label>
+            </div>
+            <div className="field is-horizontal is-expanded">
+              <div className="field is-expanded is-grouped is-grouped-right has-margin-top-xs has-margin-bottom-sm has-margin-left-auto">
+                <div className="control"><button type="submit" className="button is-primary qa-auction-bid-submit">Place Bid</button></div>
+              </div>
+            </div>
+          </form>
+        </CollapsibleSection>
+      </MediaQuery>
     </div>
   );
 }
