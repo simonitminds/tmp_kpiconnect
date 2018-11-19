037713de07b123bd82fd29658d7f38904ea6f51b
Jon <jonegeland@gmail.com>
2018-11-19 13:48:54 -0500

    Allow messages to be sent by hitting enter on the message box.

--- assets/js/components/message-panel.js
+++ assets/js/components/message-panel.js
@@ -18,33 +18,35 @@ export default class MessagePanel extends React.Component {
     }
   }
 
-  setMessage(event) {
+  handleMessageChange(event) {
     this.setState({newMessage: event.target.value})
+  }
+
+  submitMessage(event) {
+    event.preventDefault();
+    const {sendMessage, auctionId, recipientCompany} = this.props;
+    const newMessage = this.state.newMessage;
+    sendMessage(auctionId, recipientCompany, newMessage);
+    return false;
   }
 
   render() {
     const {auctionId, recipientCompany, connection, messages, sendMessage} = this.props;
     const newMessage = this.state.newMessage;
     const renderSendButton = () => {
-      if (connection) {
-        return (
-          <input
-            type="submit"
-            value='Send'
-            className='button is-turquoise'
-            onClick={sendMessage.bind(this, auctionId, recipientCompany, newMessage)}
-          ></input>
-        )
-      } else {
-        return (
-          <input
-            type="submit"
-            value='Send'
-            className='button is-disabled'
-            disabled
-          ></input>
-        )
-      }
+      const hasContent = !!newMessage;
+      const canSubmit = connection && hasContent;
+
+      const className = canSubmit ? 'button is-turquoise' : 'button is-disabled';
+
+      return (
+        <input
+          type="submit"
+          value='Send'
+          className={className}
+          disabled={!canSubmit}
+        ></input>
+      );
     }
 
     return (
@@ -73,16 +75,19 @@ export default class MessagePanel extends React.Component {
         { messages.length == 0 &&
           <div className="is-gray-0 has-text-weight-normal has-padding-md has-margin-bottom-md is-italic">No messages yet.</div>
         }
-        <div className="messaging__input">
-          <div className="field has-addons">
-            <div className="control">
-              <input className="input" placeholder='Type message here' value={newMessage} onChange={this.setMessage.bind(this)} />
-            </div>
-            <div className="control">
-              {renderSendButton()}
+
+        <form onSubmit={this.submitMessage.bind(this)}>
+          <div className="messaging__input">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" placeholder='Type message here' value={newMessage} onChange={this.handleMessageChange.bind(this)} />
+              </div>
+              <div className="control">
+                {renderSendButton()}
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     );
   }
