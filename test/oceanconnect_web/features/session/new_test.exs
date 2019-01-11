89ebe5534183138d11cd860a4682b3bddd156cbe
Cam Carter <contact@camcarter.com>
2019-01-11 15:55:16 -0500

    Good stopping point

--- test/oceanconnect_web/features/session/new_test.exs
+++ test/oceanconnect_web/features/session/new_test.exs
@@ -8,7 +8,13 @@ defmodule OceanconnectWeb.Session.NewTest do
   setup do
     user = insert(:user, password: "password", has_2fa: true)
     user_2fa = insert(:user, password: "password", has_2fa: true)
-    {:ok, %{user: user, user_2fa: user_2fa}}
+    secret =
+      :crypt.strong_rand_bytes(8)
+      |> Base.encode32()
+
+    token = :pot.hotp(secret, _num_of_trials = 1)
+
+    {:ok, %{user: user, user_2fa: user_2fa, token: token, secret: secret}}
   end
 
   test "logging in with valid user credentials", %{user: user} do
@@ -26,15 +32,14 @@ defmodule OceanconnectWeb.Session.NewTest do
     assert ForgotPasswordPage.is_current_path?()
   end
 
-  test "a user with 2fa enabled logs in with both their password and a 2fa token", %{user_2fa: user_2fa} do
+  test "a user with 2fa enabled logs in with both their password and a 2fa token", %{user_2fa: user_2fa, token: token, secret: secret} do
     NewPage.visit()
     NewPage.enter_credentials(user_2fa.email, "password")
     NewPage.submit()
 
-    secret = UUID.uuid4(:hex)
-    token = :pot.totp(secret, [timestamp: {0, 0, 10_000}])
-
     assert TwoFactorAuthPage.is_current_path?()
-    TwoFactorAuthPage.enter_credentials(sc)
+    TwoFactorAuthPage.enter_credentials(token, secret)
+
+    assert AuctionIndexPage.is_current_path?()
   end
 end
