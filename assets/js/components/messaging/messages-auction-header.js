989b714f78b021d15b28d1543a56dd4208be2fe1
Cam Carter <contact@camcarter.com>
2019-02-07 10:06:16 -0500

    Changed a word. Also message headers display correct title.

--- assets/js/components/messaging/messages-auction-header.js
+++ assets/js/components/messaging/messages-auction-header.js
@@ -4,22 +4,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AuctionTitle from '../auction/auction-title';
 
 const MessagesAuctionHeader = ({payload, onSelect}) => {
+  const {
+    auction,
+    auction_id,
+    unseen_messages,
+    status
+  } = payload;
+
   return (
     <div
-        className={`qa-auction-${payload.auction_id}-message-payloads ${payload.unseen_messages ? "with-unseen" : ""}`}
-        onClick={() => onSelect && onSelect(payload.auction_id)}>
+        className={`qa-auction-${auction_id}-message-payloads ${unseen_messages ? "with-unseen" : ""}`}
+        onClick={() => onSelect && onSelect(auction_id)}>
       { onSelect &&
         <FontAwesomeIcon icon="angle-right" className="has-padding-right-nudge" />
       }
 
-      <div className={`auction-status auction-status--${payload.status}`}>
-        {payload.status}
+      <div className={`auction-status auction-status--${status}`}>
+        {status}
       </div>
       <div>
         <AuctionTitle auction={auction} />
       </div>
-      { payload.unseen_messages > 0 &&
-        <span className="messaging__notifications qa-messages-unseen-count"><FontAwesomeIcon icon="envelope" className="has-margin-right-xs" /> {payload.unseen_messages}</span>
+      { unseen_messages > 0 &&
+        <span className="messaging__notifications qa-messages-unseen-count"><FontAwesomeIcon icon="envelope" className="has-margin-right-xs" /> {unseen_messages}</span>
       }
 
     </div>
--- assets/js/components/messaging/messages-auction-view.js
+++ assets/js/components/messaging/messages-auction-view.js
@@ -15,8 +15,8 @@ const MessagesAuctionView = ({ messagePayloads, selectedAuction, auctionState, a
   const { selectedConversation } = auctionState;
 
 
-  const auctionPayload = _.find(messagePayloads, {auction_id: selectedAuction});
-  const conversationPayload = _.find(auctionPayload.conversations, {company_name: selectedConversation});
+  const messagePayload = _.find(messagePayloads, {auction_id: selectedAuction});
+  const conversationPayload = _.find(messagePayload.conversations, {company_name: selectedConversation});
 
   return (
     <div className="messaging__context-list__container">
@@ -26,21 +26,21 @@ const MessagesAuctionView = ({ messagePayloads, selectedAuction, auctionState, a
 
       <ul>
         <li className="messaging__context-list__auction">
-          <MessagesAuctionHeader payload={auctionPayload} />
+          <MessagesAuctionHeader payload={messagePayload} />
         </li>
       </ul>
 
       { conversationPayload
         ?  <MessagePanel
               conversationPayload={conversationPayload}
-              auctionId={auctionPayload.auction_id}
+              auctionId={messagePayload.auction_id}
               connection={connection}
               actions={actions}
             />
         : <ul className="messaging__context-list qa-conversations">
             <li className="messaging__context-list__selector"><span>Select a Conversation</span></li>
             {
-              _.map(auctionPayload.conversations, (conversation) => {
+              _.map(messagePayload.conversations, (conversation) => {
                 return (
                   <li key={conversation.company_name}>
                     <MessagesConversationHeader conversation={conversation} onSelect={() => expandMessagesConversation(selectedAuction, conversation.company_name)} />
