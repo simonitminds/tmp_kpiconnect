83a9a864c082bcf4a8e3b8c40905bdcc3e572bf4
Lauren Woodrick <lauren@teamgaslight.com>
2019-04-11 17:08:27 -0400

    Address potential null returns for fuels for term auctions.

--- assets/js/components/auction/term/show/fuel-requirements-display.js
+++ assets/js/components/auction/term/show/fuel-requirements-display.js
@@ -10,23 +10,28 @@ const FuelRequirementsDisplay = (props) => {
   const showTotalFuelVolume = _.get(auction, 'show_total_fuel_volume');
   const totalFuelVolume = _.get(auction, 'total_fuel_volume');
 
-  return (
-    <ul className="list has-no-bullets">
-      <li className="is-not-flex">
-        <strong>Fuel</strong>
-        <span className="qa-auction-fuel">{fuel.name}</span>
-      </li>
-      <li className="is-not-flex">
-        <strong>Quantity</strong>
-        { showTotalFuelVolume ?
-            <div>
-              <span className="qa-auction-total_fuel_volume">{totalFuelVolume}</span> MT (<span className="qa-auction-fuel_quantity">{fuelQuantity}</span> MT/month)
-            </div>
-          :
-            <span><span className="qa-auction-fuel_quantity">{fuelQuantity}</span> MT/month</span>
-        }
-      </li>
-    </ul>
+  if(fuel) {
+    return (
+      <ul className="list has-no-bullets">
+        <li className="is-not-flex">
+          <strong>Fuel</strong>
+          <span className="qa-auction-fuel">{fuel.name}</span>
+        </li>
+        <li className="is-not-flex">
+          <strong>Quantity</strong>
+          { showTotalFuelVolume ?
+              <div>
+                <span className="qa-auction-total_fuel_volume">{totalFuelVolume}</span> MT (<span className="qa-auction-fuel_quantity">{fuelQuantity}</span> MT/month)
+              </div>
+            :
+              <span><span className="qa-auction-fuel_quantity">{fuelQuantity}</span> MT/month</span>
+          }
+        </li>
+      </ul>
+    );
+  }
+  return(
+    <i>No fuels have been specified for this auction</i>
   );
 };
 
