f6454b5ba024b99d237db56b7c0a2a6d9ce94de0
Cam Carter <contact@camcarter.com>
2019-07-24 11:04:11 -0400

    Fixed internal server error when attempting to delete a fixture

---
+++ test/support/pages/admin/auction_fixture/edit_page.ex
@@ -0,0 +1,9 @@
+defmodule Oceanconnect.Admin.Fixture.EditPage do
+  use Oceanconnect.Page
+  def delete_fixture do
+    find_element(:css, ".qa-admin-delete")
+    |> click
+
+    Hound.Helpers.Dialog.accept_dialog()
+  end
+end
